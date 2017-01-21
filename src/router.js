46916eb70ea31d67f1c0cbfc7084767fc70defd9
Juan Carlos <juancafe2@gmail.com>
2017-01-20 22:58:26 -0400

    Indexroutes and clean up

--- src/router.js
+++ src/router.js
@@ -2,14 +2,10 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import PostsIndex from './components/post_index'
 
-const Greeting = () => {
-  return <div>Hey there</div>
-}
 export default (
 <Route path="/" component={App}>
- <Route path="greet" component={Greeting}/> 
- <Route path="greet2" component={Greeting}/> 
- <Route path="greet3" component={Greeting}/> 
+ <IndexRoute component={PostsIndex} />
 </Route>
 );
