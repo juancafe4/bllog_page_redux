9107ba949c6dcae1ffa791b527577d832b153c83
Juan Carlos <juancafe2@gmail.com>
2017-01-20 23:13:50 -0400

    Post reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const FETCH_POST = 'FETCH_POST';
+export const FETCH_POSTS = 'FETCH_POSTS';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=dsvnsdinvcosinvisd';
@@ -8,7 +8,7 @@ const API_KEY = '?key=dsvnsdinvcosinvisd';
 export function fetchPost() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
   return {
-    type: FETCH_POST,
+    type: FETCH_POSTS,
     payload: request,
   }
 }
