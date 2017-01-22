13acf05fb33e7ae1f08649b5dba3feafafd902ff
Juan Carlos <juancafe2@gmail.com>
2017-01-21 23:42:34 -0400

    forms and redux-form

--- src/components/post_new.jsx
+++ src/components/post_new.jsx
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
 export default class PostNew extends Component {
   render() {
     return (
-      <div>Create Post</div>
+      <form>
+        <h3>Create a New Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control"/>
+        </div>
+
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control"/>
+        </div>
+
+        <div className="form-group">
+          <label>Content</label>
+          <textarea type="text" className="form-control"/>
+        </div>
+
+        <button type="submit" className="button button-primary">Submit</button>
+      </form>
     );
   }
 }
+
+
+export default reduxForm({
+  form: 'PostNewForm',
+  fields: ['title', 'categories', 'content'], 
+})(PostNew);
