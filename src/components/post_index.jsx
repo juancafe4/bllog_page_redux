c7c728c9ad9b5c7a98bcb359dd5e88f5a0532bc5
Juan Carlos <juancafe2@gmail.com>
2017-01-24 10:09:46 -0400

    Link blog posts with params

--- src/components/post_index.jsx
+++ src/components/post_index.jsx
@@ -14,8 +14,10 @@ class PostsIndex extends Component {
     return this.props.posts.map((post) => {
       return (
         <li key={post.id} className="list-group-item">
-          <span className="pull-xs-right">{post.categories}</span>
-          <strong>{post.title}</strong>
+          <Link to={`posts/${post.id}`}>
+            <span className="pull-xs-right">{post.categories}</span>
+            <strong>{post.title}</strong>
+          </Link>
         </li>
       );
     });
--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
-import PostsIndex from './components/post_index'
-import PostNew from './components/post_new'
+import PostsIndex from './components/post_index';
+import PostNew from './components/post_new';
+import PostsShow from './components/posts_show';
 export default (
 <Route path="/" component={App}>
  <IndexRoute component={PostsIndex} />
  <Route path="posts/new" component={PostNew} />
+ <Route path="posts/:id" component={PostsShow} />
 </Route>
 );
