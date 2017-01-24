7f9b3dfebe82cd57ecf7edf76d0b10f82e26d243
Juan Carlos <juancafe2@gmail.com>
2017-01-24 10:29:23 -0400

    Able to fetch a single post

--- src/components/posts_show.jsx
+++ src/components/posts_show.jsx
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
 
 class PostsShow extends Component {
+  componentWillMount() {
+    this.props.fetchPost(this.props.params.id);
+  }
   render() {
+    const {post} = this.props;
+    if (!this.props.post) {
+      return <div> Loading... </div>
+    }
     return (
       <div>
-        {this.props.params.id}
+        <h3>{post.title}</h3>
+        <h6>Caegories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
 }
 
-
-export default PostsShow;
+function mapStateToProps(state) {
+  return { post: state.posts.post }
+}
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
--- src/reducers/reducer_post.js
+++ src/reducers/reducer_post.js
@@ -1,9 +1,12 @@
-import { FETCH_POSTS } from '../actions/index';
+import { FETCH_POSTS, FETCH_POST } from '../actions/index';
 
 const INITIAL_STATE = { all: [], post: null };
 
-export default function(state= INITIAL_STATE, action) {
+export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
+    case FETCH_POST:
+      // Object.apply({}, state, post: action.payload.data)
+      return {...state, post: action.payload.data };
     case FETCH_POSTS:
       return { ...state ,all: action.payload.data };
     default:
