c5007b44dc8408f4d136f90278f30efc2ff71464
Juan Carlos <juancafe2@gmail.com>
2017-01-23 22:35:41 -0400

    Reux form validation

--- src/components/post_new.jsx
+++ src/components/post_new.jsx
@@ -3,39 +3,59 @@ import { Field, reduxForm } from 'redux-form';
 import { connect} from 'react-redux';
 import { createPost } from '../actions/index';
 
+const renderInput = ({meta: { touched, error} }) => (
+  <div>
+    <input className="form-control" type="text"/>
+    <div>
+      {touched && (error && <span>{error}</span>)}
+    </div>
+  </div>
+)
+const renderTextArea = ({meta: { touched, error} }) => (
+  <div>
+    <textarea className="form-control" type="text"/>
+    <div>
+      {touched && (error && <span>{error}</span>)}
+    </div>
+  </div>
+)
 let PostNew = (props) =>  {
     const { handleSubmit } = props;
-    console.log(props.createPost)
+  
     return (
       <form onSubmit={handleSubmit(props.createPost)}>
         <h3>Create a New Post</h3>
         <div className="form-group">
           <label>Title</label>
-          <Field name="title" component="input" type="text" className="form-control"/>
+          <Field name="title" component={renderInput} type="text" className="form-control"/>
         </div>
 
         <div className="form-group">
           <label>Categories</label>
-          <Field name="categories" component="input" type="text" className="form-control"/>
+          <Field name="categories" component={renderInput} type="text" className="form-control"/>
         </div>
 
         <div className="form-group">
           <label>Content</label>
-          <Field name="content" component="textarea" type="text" className="form-control"/>
+          <Field name="content" component={renderTextArea} type="text" className="form-control"/>
         </div>
 
-        <button type="submit" className="button button-primary">Submit</button>
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     );
   
 }
 
-// connect first argument is mapStateToProps second mapDispatchToProps
-// reduxForm1st is from config  2nd is mapStateToProps 3rd mapDispatchToProps 
-
+const validate = values =>  {
+  const error = {};
+  if (!values.title)
+    error.title = 'Enter a username';
+  return error;
+}
 
 PostNew = reduxForm({
   form: 'initializeFromState',
+  validate
 })(PostNew);
 
 export default connect(null, { createPost })(PostNew);
